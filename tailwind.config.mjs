4457edb46a46f66bf4704f4cf81efac947a94419
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-02-19 11:59:06 +1300

    hero & about section in home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      xs: "365px",
+      sm: "555px",
+      md: "745px",
+      lg: "935px",
+      xl: "1125px",
+    },
+    extend: {
+      maxWidth: {
+        xs: "365px",
+        sm: "555px",
+        md: "745px",
+        lg: "935px",
+        xl: "1125px",
+      },
+      colors: {
+        lightBlue: "#c3f4ff",
+        blue: "#087df1",
+        darkGrey: "#2C2C2C",
+      },
+      fontSize: {
+        "7xl": ["72px", "120%"],
+      },
+    },
+  },
+  plugins: [],
+};
