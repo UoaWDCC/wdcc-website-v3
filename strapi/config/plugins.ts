a95f50d96b20eb164e1223a1aee6ad7b2b3db8f5
clear <alexwillmcleod@gmail.com>
2024-06-21 11:03:09 +1200

    fixed plugin config

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,28 +1,28 @@
 module.exports = ({ env }) => ({
   // ...
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        s3Options: {
-          credentials: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-          },
-          region: env("AWS_REGION"),
-          endpoint: env("AWS_ENDPOINT_URL_S3"),
-          params: {
-            Bucket: env("BUCKET_NAME"),
-          },
-        },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
+  // upload: {
+  //   config: {
+  //     provider: "aws-s3",
+  //     providerOptions: {
+  //       s3Options: {
+  //         credentials: {
+  //           accessKeyId: env("AWS_ACCESS_KEY_ID"),
+  //           secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+  //         },
+  //         region: env("AWS_REGION"),
+  //         endpoint: env("AWS_ENDPOINT_URL_S3"),
+  //         params: {
+  //           Bucket: env("BUCKET_NAME"),
+  //         },
+  //       },
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       uploadStream: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
   "strapi-plugin-populate-deep": {
     config: {
       defaultDepth: 3, // Default is 5
