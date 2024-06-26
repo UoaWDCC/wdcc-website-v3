e4e4ab5b747b3c0c12be0e9d42c4466ff7b37e2a
Oculux <nate.williamson.nz@gmail.com>
2024-06-26 13:05:00 +1200

    :bug: make Strapi send authorisation header

--- web/src/lib/strapi.ts
+++ web/src/lib/strapi.ts
@@ -30,7 +30,11 @@ export default async function fetchApi<T>({
       url.searchParams.append(key, value);
     });
   }
-  const res = await fetch(url.toString());
+  const res = await fetch(url.toString(), {
+    headers: {
+      authorization: `Bearer ${import.meta.env.STRAPI_API_KEY}`,
+    },
+  });
   let data = await res.json();
 
   if (wrappedByKey) {
