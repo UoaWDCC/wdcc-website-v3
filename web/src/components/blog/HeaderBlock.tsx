5e7ded2ae9e3335600bab40ce0536b5c0c155172
Oculux <nate.williamson.nz@gmail.com>
2024-06-28 20:16:02 +1200

    :lipstick: style blog content

--- web/src/components/blog/HeaderBlock.tsx
+++ web/src/components/blog/HeaderBlock.tsx
@@ -3,7 +3,7 @@ import type { BlogHeader } from "@/types/components/BlogHeader";
 export default function HeaderBlock({ block }: { block: BlogHeader }) {
   return (
     <>
-      <h2>{block.header}</h2>
+      <h3>{block.header}</h3>
       <p>{block.description}</p>
     </>
   );
