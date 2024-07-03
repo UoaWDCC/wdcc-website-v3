e677d8955ff133b98a15ccff2e5a1f04981547ee
Chris Valenzuela <themegas100@gmail.com>
2024-07-03 19:17:27 +1200

    bugfix: Added null checking to mapToProjects

--- web/src/utils/mapToProjects.ts
+++ web/src/utils/mapToProjects.ts
@@ -2,25 +2,29 @@ import type { Project } from "@/types/types";
 const STRAPI_URL = import.meta.env.STRAPI_URL;
 
 export const extractProjectData = (json: any): Project[] => {
-  return json.map((projectData: any) => {
-    const attributes = projectData.attributes;
+  // Check if json is null or undefined before proceeding
+  if (!json) return [];
 
+  return json.map((projectData: any) => {
+    const attributes = projectData?.attributes;
+
+    // Provide default values or checks for potentially null properties
     const project: Project = {
-      id: projectData.id,
-      title: attributes.Name,
-      description: attributes.Description.map(
+      id: projectData?.id ?? '',
+      title: attributes?.Name ?? 'No Title',
+      description: attributes?.Description?.map(
         (desc: { children: { text: any }[] }) =>
           desc.children.map((child: { text: any }) => child.text).join(""),
-      ).join("\n"),
-      images: attributes.Images.data.map(
+      ).join("\n") ?? 'No Description',
+      images: attributes?.Images?.data?.map(
         (img: { attributes: { formats: { small: { url: string } } } }) =>
           STRAPI_URL + img.attributes.formats.small.url,
-      ),
-      technologies: attributes.technologies.data.map(
+      ) ?? [],
+      technologies: attributes?.technologies?.data?.map(
         (tech: { attributes: { Name: string } }) => tech.attributes.Name,
-      ),
-      demoLink: attributes.Demo[0].href,
-      viewDemoLink: attributes.Demo[0].href,
+      ) ?? [],
+      demoLink: attributes?.Demo?.[0]?.href ?? '',
+      viewDemoLink: attributes?.Demo?.[0]?.href ?? '',
     };
     return project;
   });
