dcf940a3311e783b6b29f4fde01a8532707cc364
Oculux <nate.williamson.nz@gmail.com>
2024-07-14 17:28:00 +1200

    :lipstick: style event card

--- web/src/components/events/EventCard.tsx
+++ web/src/components/events/EventCard.tsx
@@ -1,3 +1,5 @@
+import { Button } from "../ui/button";
+
 type Event = {
   title: string;
   date: Date;
@@ -6,16 +8,32 @@ type Event = {
   eventUrl: string;
 };
 
-export default function EventCard({ event }: { event: Event }) {
+export default function EventCard({
+  event,
+  className = "",
+}: {
+  event: Event;
+  className?: string;
+}) {
   const { title, date, description, imageUrl, eventUrl } = event;
 
   return (
-    <div>
-      <p>{title}</p>
-      <p>{date.toISOString()}</p>
-      <p>{description}</p>
-      <img src={imageUrl} alt={title} />
-      <a href={eventUrl}>More info</a>
+    <div
+      className={`shadow-surround rounded-md max-w-80 text-center ${className}`}
+    >
+      <img
+        src={imageUrl}
+        alt={title}
+        className="w-full aspect-[2/1] object-cover rounded-t-md shadow-bottom"
+      />
+      <div className="p-6 pb-8">
+        <h4>{title}</h4>
+        <div className="subtitle">{date.toLocaleString()}</div>
+        <p className="text-xs leading-4">{description}</p>
+        <Button asChild className="mt-6">
+          <a href={eventUrl} className="whitespace-pre">Find out more  ➢</a>
+        </Button>
+      </div>
     </div>
   );
 }
